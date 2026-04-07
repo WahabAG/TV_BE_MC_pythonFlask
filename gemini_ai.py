135751e6919d72bc3c4637319ec870f9c5c693a5
WahabAG <azeezwahab24@gmail.com>
2026-04-07 09:20:04 -0700

    adding gemini api

---
+++ gemini_ai.py
@@ -0,0 +1,44 @@
+# To run this code you need to install the following dependencies:
+# pip install google-genai
+
+import os
+from google import genai
+from google.genai import types
+from dotenv import load_dotenv
+load_dotenv()
+
+def generate(message):
+    client = genai.Client(
+        api_key=os.getenv("GEMINI_API_KEY"),
+    )
+
+    model = "gemini-3-flash-preview"
+    contents = [
+        types.Content(
+            role="user",
+            parts=[
+                types.Part.from_text(text= message),
+            ],
+        ),
+    ]
+    generate_content_config = types.GenerateContentConfig(
+        thinking_config=types.ThinkingConfig(
+            thinking_level="HIGH",
+        ),
+    )
+
+    for chunk in client.models.generate_content_stream(
+        model=model,
+        contents=contents,
+        config=generate_content_config,
+    ):
+        try:
+            if text := chunk.text:
+                return(text, end="")
+        except:
+            return("This service is Currently unavalable")
+
+if __name__ == "__main__":
+    generate()
+
+
